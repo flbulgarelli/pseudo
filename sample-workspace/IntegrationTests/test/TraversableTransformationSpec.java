eb9aaa6b3e94e886b990daa8f7a2036595bd4592
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2013-01-27 22:52:54 -0300

    Implementando algunos tests para comprensiones

--- sample-workspace/IntegrationTests/test/TraversableTransformationSpec.java
+++ sample-workspace/IntegrationTests/test/TraversableTransformationSpec.java
@@ -1,22 +1,46 @@
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 public class TraversableTransformationSpec {
 
+  private List<Persona> personas;
+  {
+    personas = Arrays.asList(new Persona() {
+      {
+        setEdad(BigDecimal.valueOf(15));
+        setNombre("pepe");
+      }
+    }, new Persona() {
+      {
+        setEdad(BigDecimal.valueOf(23));
+        setNombre("maria");
+      }
+    }, new Persona() {
+      {
+        setNombre("toto");
+        setEdad(BigDecimal.valueOf(44));
+      }
+    });
+  }
+
   @Test
-  public void soporta_map() throws Exception {
+  public void soporta_map() throws Throwable {
+    assertEquals(Arrays.asList("pepe", "maria", "toto"), new Comprensiones().nombres_de_personas(personas));
+  }
+
+  @Test
+  public void soporta_filter() throws Throwable {
     fail();
   }
 
   @Test
-  public void soporta_filter() throws Exception {
-    fail();
-  }
-
-  @Test
-  public void soporta_filter_con_map() throws Exception {
-    fail();
+  public void soporta_filter_con_map() throws Throwable {
+    assertEquals(Arrays.asList("pepe", "maria", "toto"), new Comprensiones().nombres_de_personas_mayores(personas));
   }
 
   @Test
@@ -40,13 +64,13 @@ public class TraversableTransformationSpec {
   }
 
   @Test
-  public void soporta_minimo_segun_criteio() throws Exception {
-    fail();
+  public void soporta_minimo_segun_criteio() throws Throwable {
+    assertEquals(personas.get(0), new Comprensiones().persona_mas_joven(personas));
   }
 
   @Test
-  public void soporta_maximo_segun_criterio() throws Exception {
-    fail();
+  public void soporta_maximo_segun_criterio() throws Throwable {
+    assertEquals(personas.get(2), new Comprensiones().persona_mas_adulta(personas));
   }
 
   @Test
