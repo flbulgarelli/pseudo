5b29181caebfc93d4cec869369e412d12bd94e9d
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2013-01-29 19:59:50 -0300

    Agregando validaciones basicas de tipos

--- org.uqbarproject.pseudo/src/org/uqbarproject/pseudo/validation/PseudoJavaValidator.java
+++ org.uqbarproject.pseudo/src/org/uqbarproject/pseudo/validation/PseudoJavaValidator.java
@@ -1,5 +1,7 @@
 package org.uqbarproject.pseudo.validation;
 
+import static org.uqbarproject.pseudo.pseudo.PseudoPackage.Literals.*;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -7,13 +9,23 @@ import java.util.Set;
 
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.xtext.validation.Check;
-import org.uqbarproject.pseudo.pseudo.Member;
+import org.uqbarproject.pseudo.pseudo.Attribute;
+import org.uqbarproject.pseudo.pseudo.CollectionLiteralExpression;
+import org.uqbarproject.pseudo.pseudo.ConstructionExpression;
+import org.uqbarproject.pseudo.pseudo.DecrementExpression;
+import org.uqbarproject.pseudo.pseudo.Expression;
+import org.uqbarproject.pseudo.pseudo.FalseExpression;
+import org.uqbarproject.pseudo.pseudo.IdExpression;
+import org.uqbarproject.pseudo.pseudo.IncrementExpression;
 import org.uqbarproject.pseudo.pseudo.LocalVariable;
+import org.uqbarproject.pseudo.pseudo.Member;
 import org.uqbarproject.pseudo.pseudo.Method;
 import org.uqbarproject.pseudo.pseudo.Parameter;
-import org.uqbarproject.pseudo.pseudo.PseudoPackage;
 import org.uqbarproject.pseudo.pseudo.Statement;
+import org.uqbarproject.pseudo.pseudo.StringExpression;
+import org.uqbarproject.pseudo.pseudo.TrueExpression;
 import org.uqbarproject.pseudo.pseudo.Type;
 
 public class PseudoJavaValidator extends AbstractPseudoJavaValidator {
@@ -57,7 +69,7 @@ public class PseudoJavaValidator extends AbstractPseudoJavaValidator {
       }
 
       protected EAttribute feature() {
-        return PseudoPackage.Literals.MEMBER__NAME;
+        return MEMBER__NAME;
       }
 
       protected String duplicationMessage() {
@@ -79,7 +91,7 @@ public class PseudoJavaValidator extends AbstractPseudoJavaValidator {
       }
 
       protected EAttribute feature() {
-        return PseudoPackage.Literals.LOCAL_VARIABLE__NAME;
+        return LOCAL_VARIABLE__NAME;
       }
 
       protected String duplicationMessage() {
@@ -93,11 +105,11 @@ public class PseudoJavaValidator extends AbstractPseudoJavaValidator {
   @Check
   public void checkDuplicateParameters(Method method) {
     Set<String> parameterNames = new HashSet<String>();
-    for(Parameter parameter : method.getParameters()) {
+    for (Parameter parameter : method.getParameters()) {
       parameterNames.add(parameter.getName());
     }
     if (parameterNames.size() != method.getParameters().size()) {
-      error("Duplicate parameters", method, PseudoPackage.Literals.METHOD__PARAMETERS, INSIGNIFICANT_INDEX);
+      error("Duplicate parameters", method, METHOD__PARAMETERS, INSIGNIFICANT_INDEX);
     }
   }
 
@@ -106,25 +118,100 @@ public class PseudoJavaValidator extends AbstractPseudoJavaValidator {
   @Check
   public void checkTypeIsUppercase(Type type) {
     if (!Character.isUpperCase(type.getName().charAt(0))) {
-      warning(
-        "Classes should start with an uppercase letter",
-        type,
-        PseudoPackage.Literals.TYPE__NAME,
-        INSIGNIFICANT_INDEX);
+      warning("Classes should start with an uppercase letter", type, TYPE__NAME, INSIGNIFICANT_INDEX);
     }
   }
 
   @Check
   public void checkMethodIsLowercase(Method method) {
     if (!Character.isLowerCase(method.getName().charAt(0))) {
-      warning(
-        "Methods should start with a lower case letter",
-        method,
-        PseudoPackage.Literals.METHOD__CLASS_METHOD,
-        INSIGNIFICANT_INDEX);
+      warning("Methods should start with a lower case letter", method, METHOD__CLASS_METHOD, INSIGNIFICANT_INDEX);
     }
   }
 
-  /* Type inference warnings */
+  /* Type guess - not really inference - warnings */
 
+  abstract class NumericUpdateChecker<T extends EObject> {
+
+    void check(T update) {
+      checkUpdatedVariableMayBeNumeric(update, getTarget(update), targetFeature());
+      if (getValue(update) != null)
+        checkUpdatingExpressionMayBeNumeric(update, getValue(update), valueFeature());
+    }
+
+    protected abstract EReference valueFeature();
+
+    protected abstract EReference targetFeature();
+
+    protected abstract Expression getValue(T update);
+
+    protected abstract EObject getTarget(T update);
+
+    private void checkUpdatedVariableMayBeNumeric(EObject update, EObject target, EReference feature) {
+      if (target instanceof Attribute)
+        checkMayBeNumeric(update, ((Attribute) target).getInitialValue(), feature);
+      else if (target instanceof LocalVariable)
+        checkMayBeNumeric(update, ((LocalVariable) target).getValue(), feature);
+    }
+    
+    private void checkUpdatingExpressionMayBeNumeric(EObject update, Expression target, EReference feature) {
+      if (target instanceof IdExpression)
+        checkUpdatedVariableMayBeNumeric(update, ((IdExpression) target).getValue(), feature);
+      else 
+        checkMayBeNumeric(update, (Expression) target, feature);
+    }
+
+    private void checkMayBeNumeric(EObject update, Expression target, EReference feature) {
+      if (target instanceof ConstructionExpression //
+        || target instanceof CollectionLiteralExpression //
+        || target instanceof StringExpression //
+        || target instanceof TrueExpression //
+        || target instanceof FalseExpression) {
+        warning("Expression does not seem to be numeric", update, feature, INSIGNIFICANT_INDEX);
+      }
+    }
+
+  }
+
+  @Check
+  public void checkIncrementedVariableIsNumber(IncrementExpression increment) {
+    new NumericUpdateChecker<IncrementExpression>() {
+      protected EReference valueFeature() {
+        return INCREMENT_EXPRESSION__VALUE;
+      }
+
+      protected EReference targetFeature() {
+        return INCREMENT_EXPRESSION__TARGET;
+      }
+
+      protected Expression getValue(IncrementExpression update) {
+        return update.getValue();
+      }
+
+      protected EObject getTarget(IncrementExpression update) {
+        return update.getTarget();
+      }
+    }.check(increment);
+  }
+
+  @Check
+  public void checkDecrementedVariableIsNumber(DecrementExpression decrement) {
+    new NumericUpdateChecker<DecrementExpression>() {
+      protected EReference valueFeature() {
+        return DECREMENT_EXPRESSION__VALUE;
+      }
+
+      protected EReference targetFeature() {
+        return DECREMENT_EXPRESSION__TARGET;
+      }
+
+      protected Expression getValue(DecrementExpression update) {
+        return update.getValue();
+      }
+
+      protected EObject getTarget(DecrementExpression update) {
+        return update.getTarget();
+      }
+    }.check(decrement);
+  }
 }
